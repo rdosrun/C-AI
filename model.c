0d8ec084de504099f19cbeb3ceae8414711ebc7d
rdosrun <rdosrun@gmail.com>
2025-08-18 20:25:18 -0600

    general completed bugs still need to be resovled

--- model.c
+++ model.c
@@ -5,8 +5,9 @@
 #define VOCAB_SIZE 32
 #define EMBEDDING_SIZE  1
 #define CLASS_COUNT 4
-#define TRAINING_ITR  5
+#define TRAINING_ITR  500
 #define INPUT_FILE "fizzbuzz.txt"
+#define LR .01
 
 
 void fill_matrix_with_hashtable(HashTable *ht, struct matrix *m){
@@ -87,49 +88,66 @@ int main(int argc, char ** argv){
     for(int i =0;i<TRAINING_ITR;++i){
         //data set loop
         //
+        double loss_sum = 0.0;
         printf("=======loop %d========\n",i);
         for(int j =0;j<10000;++j){
                //forward prop
-            printf("  ====forward prop====\n");
+            //printf("  ====forward prop====\n");
             input_layer = encode_input(j,32);
             if (hidden_layer1){
                 destroy_matrix(hidden_layer1);
                 hidden_layer1 = NULL;
             }
-            printf("transpose\n");
+            //printf("transpose\n");
             hidden_layer1 = dot(transpose(embedding_layer),input_layer);
             relu(hidden_layer1);
-            printf("completed relu\n");
+            //printf("completed relu\n");
             if(logits){
                 destroy_matrix(logits);
                 logits = NULL;
             }
-            printf("dot\n");
+            //printf("dot\n");
             logits = dot(weight,hidden_layer1);
             if(probs){
                 destroy_matrix(probs);
                 probs = NULL;
             }
             probs = add_inplace(logits, bias_term);
-            printf("completed bias term\n");
+            //printf("completed bias term\n");
 
             Softmax(probs);
-            printf("completed Softmax \n");
-
+            //printf("completed Softmax \n");
+            printf("guess for %d\n",j);
             print_matrix(probs);
 
             struct matrix * one_hot = generate_answer(j);
 
+            double eps = 1e-12, L = 0.0;
+            for (int c = 0; c < CLASS_COUNT; ++c)
+                if (one_hot->grid[c] > 0.0) L -= log(logits->grid[c] + eps);
+            loss_sum += L;
+
             //back prop
-            printf("  ====back prop====\n");
+            //printf("  ====back prop====\n");
             dlogits = sub_inplace(probs,one_hot);
             dW = dot(dlogits,transpose(hidden_layer1));
             dh = dot(transpose(weight),dlogits);
+            relu(dh);
+            //printf("====== completed back prop=========\n");
 
+
+            for (size_t i = 0, nW = (size_t)weight->height * weight->width; i < nW; ++i)
+                weight->grid[i] -= LR  * dW->grid[i];
+
+
+            //printf("====== updated weight=========\n");
+            // b -= LR * db
+            for (int i = 0; i < CLASS_COUNT; ++i)
+                bias_term->grid[i] -= LR * dlogits->grid[i];
 
 
         }
-
+        printf("avg loss: %.6f\n",  loss_sum / 10000);
 
 
     }
